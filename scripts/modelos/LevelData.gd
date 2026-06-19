c7c948bfd1cbfa7de3876faf91c362bdc9a69b1d
Javier Dinamarca <javierdinamarcafredes@gmail.com>
2026-06-19 18:54:05 -0400

    avance de los niveles y selector

---
+++ scripts/modelos/LevelData.gd
@@ -0,0 +1,6 @@
+# LevelData.gd
+class_name LevelData extends Resource
+
+@export var level_name: String
+@export var scene_path: String # Ruta hacia el archivo .tscn del nivel
+@export var is_locked: bool = true
